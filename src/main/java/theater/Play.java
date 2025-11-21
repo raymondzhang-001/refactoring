605bf0f6f180d548a6bbc394e11a00448343ad5b
raymondzhang-001 <ramon.zhang@mail.utoronto.ca>
2025-11-21 10:35:39 +0800

    finish Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,29 @@
 package theater;
 
+/**
+ * Represents a play with a name and type (e.g., tragedy or comedy).
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private final String name;
+    private final String type;
 
+    /**
+     * Creates a new Play object.
+     *
+     * @param name the name of the play
+     * @param type the type of the play (e.g., "tragedy", "comedy")
+     */
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
